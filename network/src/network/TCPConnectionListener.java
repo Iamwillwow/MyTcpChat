206a3d1b5d4da5712ea682b873333bc4e2da510e
dimas <dimasemin.98@gmail.com>
2019-08-29 15:38:22 +0300

    Initial commit

---
+++ network/src/network/TCPConnectionListener.java
@@ -0,0 +1,10 @@
+package network;
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onDisconnect(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection,String value);
+    void onException(TCPConnection tcpConnection,Exception e);
+
+}
