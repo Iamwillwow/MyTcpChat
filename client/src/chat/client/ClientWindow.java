bd2b642de8c8eeeb880a75a28f1a619eb43dcdff
Iamwillwow <48827957+Iamwillwow@users.noreply.github.com>
2019-08-29 15:39:26 +0300

    Update ClientWindow.java
    change ip address

--- client/src/chat/client/ClientWindow.java
+++ client/src/chat/client/ClientWindow.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
 
-    private static final String IP_ADDR = "192.168.56.1";
+    private static final String IP_ADDR = "192.168.1.1";
     private static final int PORT = 8189;
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
